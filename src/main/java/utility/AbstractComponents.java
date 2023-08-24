7f03959ff7fdd3eb1b273955179977794c783a8d
Adderly Mejía <addrmh@gmail.com>
2023-08-24 12:24:30 -0500

    Setting abstract components for web actions

---
+++ src/main/java/utility/AbstractComponents.java
@@ -0,0 +1,30 @@
+package utility;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AbstractComponents {
+
+    WebDriver driver;
+
+    public AbstractComponents(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitVisibilityOfElementLocated(By findBy) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(findBy));
+    }
+
+    public Alert waitAndSwitchToAlert() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.alertIsPresent());
+        return driver.switchTo().alert();
+    }
+
+}
