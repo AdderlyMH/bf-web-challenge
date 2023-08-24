3eb04be0bd4250136054ffbe3f6ef462e3d6531b
Adderly Mejía <addrmh@gmail.com>
2023-08-24 12:25:49 -0500

    Setting definitions for first test case steps

---
+++ src/test/java/stepdefinition/StepDefinition.java
@@ -0,0 +1,44 @@
+package stepdefinition;
+
+import components.TestBase;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.MainPage;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
+
+public class StepDefinition extends TestBase {
+
+    public MainPage mainPage;
+
+    @Given("user goes to the main page")
+    public void userGoesToTheMainPage() throws IOException {
+        mainPage = startApp();
+    }
+
+    @Given("user clicks the header SIGN UP button")
+    public void userClicksTheHeaderSIGNUPButton() {
+        mainPage.clickSignUpHeaderButton();
+    }
+
+    @Given("user writes data: {string}, {string}")
+    public void userWritesData(String username, String password) {
+        mainPage.writeSignUpUsernameInput(username);
+        mainPage.writeSignUpPasswordInput(password);
+    }
+
+    @When("user clicks the modal SIGN UP button")
+    public void userClicksTheModalSIGNUPButton() {
+        mainPage.clickSignUpModalButton();
+    }
+
+    @Then("appears a pop-up with the message {string}")
+    public void appearsAPopUpWithTheMessage(String expectedAlertMessage) {
+        String actualAlertMessage = mainPage.getAlertText();
+        assertEquals(actualAlertMessage, expectedAlertMessage,"Alert messages don't match");
+    }
+
+}
