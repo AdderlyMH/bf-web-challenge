026a28f2a7729d696879c49032f3af156e25bfb6
Adderly Mejía <addrmh@gmail.com>
2023-08-25 14:44:46 -0500

    Updating case

--- src/test/java/standalone/SignUpHappyPathTests.java
+++ src/test/java/standalone/SignUpHappyPathTests.java
@@ -11,12 +11,12 @@ import static org.testng.Assert.assertEquals;
 public class SignUpHappyPathTests extends TestBase {
 
     @Test(dataProvider = "getMap")
-    public void createUserTest(HashMap<String,Object> parameter) {
+    public void createUserTest(HashMap<String,String> parameter) {
 
         mainPage.clickSignUpHeaderButton();
 
-        mainPage.writeSignUpUsernameInput((String) parameter.get("username"));
-        mainPage.writeSignUpPasswordInput((String) parameter.get("password"));
+        mainPage.writeSignUpUsernameInput(parameter.get("username"));
+        mainPage.writeSignUpPasswordInput(parameter.get("password"));
 
         mainPage.clickSignUpModalButton();
 
@@ -31,12 +31,12 @@ public class SignUpHappyPathTests extends TestBase {
     public Object[][] getMap() {
 
         HashMap<String,Object> firstMap = new HashMap<>();
-        firstMap.put("username", "another155");
+        firstMap.put("username", "another255");
         firstMap.put("password", "another15");
         firstMap.put("expectedAlertMessage", "Sign up successful.");
 
         HashMap<String,Object> secondMap = new HashMap<>();
-        secondMap.put("username", "another166");
+        secondMap.put("username", "another366");
         secondMap.put("password", "another16");
         secondMap.put("expectedAlertMessage", "Sign up successful.");
 
